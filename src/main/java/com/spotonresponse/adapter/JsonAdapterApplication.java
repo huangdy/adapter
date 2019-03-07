13ee6f264e2741f823a8bb37bfc8efaab21c85ee
huangdy <coololdman@gmail.com>
2019-03-07 09:08:37 -0500

    check-in version for upload

--- src/main/java/com/spotonresponse/adapter/JsonAdapterApplication.java
+++ src/main/java/com/spotonresponse/adapter/JsonAdapterApplication.java
@@ -1,15 +1,20 @@
 package com.spotonresponse.adapter;
 
+import com.spotonresponse.adapter.config.FileStorageProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaRepositories
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class JsonAdapterApplication {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
