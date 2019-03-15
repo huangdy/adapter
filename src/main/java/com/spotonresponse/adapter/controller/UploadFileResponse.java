6096638a8be3b13c3efd97bb279ea05a9e9d7d6f
huangdy <coololdman@gmail.com>
2019-03-15 10:27:07 -0400

    to make response work

--- src/main/java/com/spotonresponse/adapter/controller/UploadFileResponse.java
+++ src/main/java/com/spotonresponse/adapter/controller/UploadFileResponse.java
@@ -1,6 +1,7 @@
 package com.spotonresponse.adapter.controller;
 
 public class UploadFileResponse {
+
     private String fileName;
     private String fileDownloadUri;
     private String fileType;
@@ -14,5 +15,44 @@ public class UploadFileResponse {
         this.size = size;
     }
 
-    // Getters and Setters (Omitted for brevity)
+    public String getFileName() {
+
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+
+        this.fileName = fileName;
+    }
+
+    public String getFileDownloadUri() {
+
+        return fileDownloadUri;
+    }
+
+    public void setFileDownloadUri(String fileDownloadUri) {
+
+        this.fileDownloadUri = fileDownloadUri;
+    }
+
+    public String getFileType() {
+
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+
+        this.fileType = fileType;
+    }
+
+    public long getSize() {
+
+        return size;
+    }
+
+    public void setSize(long size) {
+
+        this.size = size;
+    }
 }
+
